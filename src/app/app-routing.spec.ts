c2866fa01203330775e5593c7fbdb53885d75197
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2019-04-18 09:02:06 -0300

    Created tests for routing

---
+++ src/app/app-routing.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, async, fakeAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes, AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+describe('AppRoutingModule', () => {
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                AppRoutingModule
+            ]
+        });
+    }));
+
+    it(`should not provide router module`, () => {
+        expect(() => TestBed.get(RouterModule)).toThrowError();
+    });
+});
+
+describe('AppRoutingModule.forRoot()', () => {
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes(routes),
+                AppRoutingModule
+            ],
+            declarations: [
+                LoginComponent
+            ]
+        })
+    }));
+
+    it(`should create AppRoutingModule`, () => {
+        expect(TestBed.get(AppRoutingModule)).not.toBeNull();
+    });
+
+    it(`should redirect to login when clicking on login button`, fakeAsync(() => {
+        const router = TestBed.get(RouterModule);
+        expect(router).not.toBeNull();
+    }));
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,41 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { routes } from './app-routing.module';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 describe('AppComponent', () => {
   let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         MaterialModule,
         FlexLayoutModule,
         NoopAnimationsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent
       ],
+      providers: []
     }).compileComponents().then(() => {
       fixture = TestBed.createComponent(AppComponent);
       app = fixture.debugElement.componentInstance;
+      router = TestBed.get(Router);
+      fixture.ngZone.run(() => {
+        router.initialNavigation();
+      });
     });
   }));
+
 
   it('should create the app', () => {
     expect(app).toBeTruthy();
